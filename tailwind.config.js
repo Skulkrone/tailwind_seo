98016673d7ff2451fa9ba40893bfdc6d8174557b
Skulkrone <40754599+Skulkrone@users.noreply.github.com>
2022-09-19 17:03:50 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      gridTemplateColumns : {
+        "menu" : "100px 1fr 100px"
+      }
+    },
+  },
+  plugins: [],
+}
